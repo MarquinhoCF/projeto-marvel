1c4a0a6c679fb75af972dc5046af3b1c2809de85
MarquinhoCF <marcos13cf@gmail.com>
2022-05-17 13:56:41 -0300

    Finalizando o projeto com JavaScript

---
+++ src/js/index.js
@@ -0,0 +1,41 @@
+const personagens = document.querySelectorAll('.personagem');
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+
+        const idSelecionado = personagem.attributes.id.value;
+
+        if (idSelecionado == 'ultron') return;
+
+        /* 
+            OBJETIVO 1 - quando passar o mouse em cima do persongens da listagem
+            temos que:
+
+            --> colocar a classe selecionado no personagem que passamos o mouse
+            por cima para adicionar a animação nele
+
+            --> retirar a classe selecionado do objeto que estava selecionado antes
+        */
+        const personagemSelecionado = document.querySelector('.selecionado');
+        personagemSelecionado.classList.remove('selecionado');
+
+        personagem.classList.add('selecionado');
+
+        /* 
+            OBJETIVO 2 - quando passar o mouse por cima do personagem da listagem, 
+            trocar a imagem do personagem grande
+
+            --> alterar a imagem do jogador 1
+
+            --> alterar o nome do jogador 1
+        */
+        
+        const imagemJogador1 = document.getElementById('personagem-jogador-1');
+        imagemJogador1.src = `./src/imagens/${idSelecionado}.png`;
+
+        const nomeJogador1 = document.getElementById('nome-jogador-1');
+        const nomeSelecionado = personagem.getAttribute('data-name');
+
+        nomeJogador1.innerHTML = nomeSelecionado;
+    });
+});
